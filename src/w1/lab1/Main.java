2630ad65b19804092b2f813c26b1db74bdd02242
jsh0409100 <jsh0409100@gmail.com>
2021-09-13 01:40:22 +0900

    Completed!

--- src/w1/lab1/Main.java
+++ src/w1/lab1/Main.java
@@ -1,5 +1,30 @@
 package w1.lab1;
 
-public class Main {
+import java.util.ArrayList;
 
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<Student> list = new ArrayList<Student>();
+		
+		Student s = new Student();
+		s.setNo(1);
+		s.setName("saehee");
+		s.setAge(20);
+		list.add(s);
+		
+		list.add(new Student(2, "Youjin", 22));
+		
+		System.out.println("size of the list: " + list.size());
+		System.out.println("\n <List of students>");
+		for(int i = 0; i<list.size(); i++) {
+			System.out.println(list.get(i).toString());	
+		}
+		
+		System.out.println("\n <Removed result>");
+		list.remove(1);
+		for(Student ss: list) {
+			System.out.println(ss.toString());	
+		}
+		
+	}
 }
--- src/w1/lab1/Student.java
+++ src/w1/lab1/Student.java
@@ -1,5 +1,41 @@
 package w1.lab1;
 
 public class Student {
-
+	private int no;
+	private String name;
+	private int age;
+	
+	public Student() {}
+	public Student(int no, String name, int age) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.age = age;
+	}
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return no + "-" + name + " (" + age + ")";
+	}
+	
+	
 }
